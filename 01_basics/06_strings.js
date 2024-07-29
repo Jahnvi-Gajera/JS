327efe2f0f8ca2d22d8dd098373a2cbe8fd08a79
Jahnvi-Gajera <155662998+Jahnvi-Gajera@users.noreply.github.com>
2024-07-29 17:45:05 +0000

    stringg

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -1,14 +1,38 @@
-const name = "Jal"
-const repoCount = 50
+// const name = "Jal"
+// const repoCount = 50
 
-console.log(name +repoCount + "value");         //Outdated syntax
+// console.log(name +repoCount + "value");         //Outdated syntax
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('Jahnvi')    //Another way of declaring string    //String is an object
+const gameName = new String('Jahnvi-JG-com')    //Another way of declaring string    //String is an object
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
-console.log(gameName.length);
-console.log(gameName.toLowerCase());
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(0));
+// console.log(gameName.indexOf('O'));
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(0,4);
+// const str2 = gameName.slice(-8,4);
+// const newString = gameName.substring(-8,4);
+// console.log(anotherString);
+// console.log(str2);
+// console.log(newString);
+
+// const newString1 = "  Jahnvi  "
+// console.log(newString1);
+// console.log(newString1.trim());
+
+const url = "https://jahnvi.com/jahnvih%20gajera"
+
+console.log(url.replace('%20','-'));
+console.log(url.includes('jahnvi'));
+console.log(url.includes('dev'));
+
+console.log(gameName.split('-'));
