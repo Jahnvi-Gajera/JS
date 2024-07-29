b19dedb4bb299b12b75bded10c128cf45918017d
Jahnvi-Gajera <155662998+Jahnvi-Gajera@users.noreply.github.com>
2024-07-29 03:56:50 +0000

    str

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,14 @@
+const name = "Jal"
+const repoCount = 50
+
+console.log(name +repoCount + "value");         //Outdated syntax
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Jahnvi')    //Another way of declaring string    //String is an object
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
