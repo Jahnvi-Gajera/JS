080ac1c29ba2566206aa3669dc35f72a48a43498
Jahnvi-Gajera <155662998+Jahnvi-Gajera@users.noreply.github.com>
2024-07-31 12:54:12 +0000

    num and math

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,30 @@
+// const score = 400
+// console.log(score);
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+ const otherNumber = 213.8966
+// console.log(otherNumber.toPrecision(3));
+
+const hund = 1000000
+// console.log(hund.toLocaleString('en-IN'));
+
+// ***************************Maths**********************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,67,8,2));
+// console.log(Math.max(23,888,999,578));
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random()*10 + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1))+10)
