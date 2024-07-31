2b3e847c5e92c106f9c7e68c4d418236fe5840d5
Jahnvi-Gajera <155662998+Jahnvi-Gajera@users.noreply.github.com>
2024-07-31 17:14:02 +0000

    arrayyyys

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,38 @@
+// arrays
+
+ const myArray = [0,1,2,3,4,5]
+// const myHeroes = ["Shaktiman","naagraj"]
+
+// const myarr2 = new Array(1,2,3,4)   // Another way of declaring array
+// console.log(myArray[0]);
+// console.log(myHeroes[1]);
+
+// Array methods
+
+// myArray.push(6)
+// myArray.pop()
+// myArray.unshift(9)            //insert element at starting
+// myArray.shift()               //remove  "      "    "    "
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(2));
+// console.log(myArray.indexOf(9));
+
+// const newArray = myArray.join()
+
+// console.log(myArray);
+// console.log(newArray);
+//console.log(typeof newArray);
+
+
+// slice,splice
+
+console.log("A",myArray);
+
+const myA1 = myArray.slice(1,3)
+console.log(myA1);
+console.log("B",myArray);
+
+const myA2 = myArray.splice(1,3)
+console.log(myA2);
+console.log("B",myArray);
+
