93c096ce4db32e27cbdb13cc7ac0fcfc0195650a
Jahnvi-Gajera <155662998+Jahnvi-Gajera@users.noreply.github.com>
2024-08-04 16:03:04 +0000

    while & do while

---
+++ 05_iterations/while.js
@@ -0,0 +1,25 @@
+//while loop
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index +2
+// }
+
+let myArray = ['flash','batman','spiderman']
+
+// let a = 0
+// while (a <myArray.length) {
+//     console.log(`value is ${myArray[a]}`);
+//     a = a + 1
+// }
+
+// do while loop
+
+//let score = 1
+let score = 11                     //only prints 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
