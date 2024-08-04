941fbd01b58b233286839156625f616829e81ddd
Jahnvi-Gajera <155662998+Jahnvi-Gajera@users.noreply.github.com>
2024-08-04 11:44:12 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,45 @@
+//Switch
+
+const month = "march"
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    case 9:
+        console.log("sep");
+        break;
+    case 10:
+        console.log("oct");
+        break;
+    case 11:
+        console.log("nov");
+        break;
+    case 12:
+        console.log("dec");
+        break;
+    default:
+        console.log("Enter valid value");
+        break;
+}
